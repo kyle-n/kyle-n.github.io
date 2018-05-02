6559a9fd29cf1ed02cefcff7f78adffc89122763
kyle-n <kylebnazario@gmail.com>
2018-05-02 13:44:41 -0400

    Javascript and cleanup updates
    - Put JS in external file to follow best practices
    - Cleaned up some minor mistakes in the JS
    - Fixed Twitter card issue, should have been site var
    - New version of site without jQuery waiting until I better understand
      CSS and basic JS animations

--- _site/siteUI.js
+++ _site/siteUI.js
@@ -1,30 +1,61 @@
-//Wait to load
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
 
-	 //Grab background color settings, if they exist
-	 var nightIn = '';
-	 var night = false;
-	 var doc = document.querySelector('html');
-	 var nightButton = document.querySelector('#nightmode');
-	 try {
-        nightIn = localStorage.getItem('nightMode');
+	//Grab background color settings, if they exist
+	var nightIn = '';
+	var night = false;
+	try {
+		nightIn = localStorage.getItem('nightMode');
 
-		  //Set night mode on page load
-        if (nightIn === 'y') {
-            night = true;
-				doc.style['background-color'] = 'black';
-				doc.style['color'] = 'white';
-				nightButton.innerHTML('&#9728;');
-        }
-	 }
-	 catch (error) {
-        localStorage.setItem('nightMode', 'n')
-	 }
+		//Set night mode on page load
+		if (nightIn === 'y') {
+			night = true;
+			$('html').css('background-color', 'black');
+			$('html').css('color', 'white');
+			$('#nightmode').html('&#9728;');
+		}
+	}
+	catch (error) {
+		localStorage.setItem('nightMode', 'n')
+	}
 
-	 //Toggle footnotes when button is clicked
-	 var visible = false;
-    document.querySelector('.fn_button').addEventListener('click', 
-      function() {
-        
-	 });
+	//Toggle footnotes when button is clicked
+	var visible = false;
+	$('.fn_button').on('click', function() {
+		var $fn = $(this).next();
+		if (visible) {
+			$fn.fadeOut();
+			visible = false;
+		}
+		else {
+			$fn.fadeIn();
+			$fn.css('display', 'block');
+			visible = true;
+		}
+	});
+
+	//Background color settings
+	$('#nightmode').on('click', function() {
+
+		//Night is on, turn it off
+		if (night) {
+			$('html').css('background-color', '#fdfdfd');
+			$('html').css('color', 'black');
+			$(this).html('&#9790;');
+
+			//Set var and memory
+			night = false;
+			localStorage.setItem('nightMode', 'n');
+		}
+
+		//Night is off, turn it on
+		else {
+			$('html').css('background-color', 'black');
+			$('html').css('color', 'white');
+			$(this).html('&#9728;');
+
+			//Set var and memory
+			night = true;
+			localStorage.setItem('nightMode', 'y');
+		}
+	});
 });
