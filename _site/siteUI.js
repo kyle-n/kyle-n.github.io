2021626d7adc5c1d33915ca4451a2550f8d1e1c9
kyle-n <kylebnazario@gmail.com>
2018-06-12 15:49:59 -0400

    Footnotes and night mode work without jQuery

--- _site/siteUI.js
+++ _site/siteUI.js
@@ -1,14 +1,38 @@
 //On DOM load (Before images and all other scripts
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    
+    //Store common elements for quick reference
+    var page = document.querySelector('html');
+    var nightModeButton = document.querySelector('#nightmode');
+    var blockquoteParagraphs = document.querySelector('blockquote p');
+    
+    //Change page css for light text, dark background
+    const goNight = function () {
+        page.style.backgroundColor = 'black';
+        page.style.color = 'white';
+        nightModeButton.innerHTML = '&#9728;';
+        if (blockquoteParagraphs != null) blockquoteParagraphs.style.color = 'black';
+        night = true;
+        localStorage.setItem('nightMode', 'y');
+    };
+    
+    //Change page css for light background, dark text
+    const goDay = function () {
+        page.style.backgroundColor = '#fdfdfd';
+        page.style.color = 'black';
+        nightModeButton.innerHTML = '&#9790;';
+        night = false;
+        localStorage.setItem('nightMode', 'n');
+    };
 
 	//Grab background color settings, if they exist
 	var nightIn = '';
 	var night = false;
 	try {
-		nightIn = localStorage.getItem('nightMode');
-
-		//Set night mode on page load
-		if (nightIn === 'y') {
+        //Set night mode on load if it was selected
+        let isNight = localStorage.getItem('nightMode');
+        console.log('night', isNight)
+		if (isNight == 'y') {
 			goNight();
 		}
 	}
@@ -18,51 +42,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	//Toggle footnotes when button is clicked
 	var visible = false;
-	document.querySelector('.fn_button').addEventListener('click', 
-		function(e) {
-		var fn = this.nextSibling;
-		if (visible) {
-			fn.classList.remove('fn_text_showing');
-			fn.classList.add('fn_text_hidden');
-			visible = false;
-		}
-		else {
-			fn.classList.remove('fn_text_hidden');
-			fn.classList.add('fn_text_showing');
-			visible = true;
-		}
-	});
-
-	//Background color settings
-	$('#nightmode').on('click', function() {
-		if (night) {
-			goDay();
-		}
-		else {
-			goNight();
-		}
-	});
-
-	//Activate night mode
-	function goNight() {
-		$('html').css('background-color', 'black')
-			.css('color', 'white');
-		$('#nightmode').html('&#9728;');
-		$('blockquote p').css('color', 'black');
-		night = true;
-		localStorage.setItem('nightMode', 'y');
+	var footnotes = document.querySelectorAll('.fn_button');
+	if (footnotes != null) {
+        for (let i = 0; i < footnotes.length; i++) {
+            footnotes[i].addEventListener('click', function () {
+                var fn = this.nextSibling;
+                if (visible) {
+                    fn.classList.remove('fn_text_showing');
+                    fn.classList.add('fn_text_hidden');
+                    visible = false;
+                }
+                else {
+                    fn.classList.remove('fn_text_hidden');
+                    fn.classList.add('fn_text_showing');
+                    visible = true;
+                }
+            });
+        }
 	}
+    
+    //Night mode toggle button
+    nightModeButton.addEventListener('click', function () {
+        if (night) goDay();
+        else goNight();
+    });
 
-	//Activate day mode
-	function goDay() {
-			$('html').css('background-color', '#fdfdfd')
-				.css('color', 'black');
-			$('#nightmode').html('&#9790;');
-			night = false;
-			localStorage.setItem('nightMode', 'n');
-	}
-	/*
-	 //Toggle footnotes when button is clicked
-	 */
 });
 
