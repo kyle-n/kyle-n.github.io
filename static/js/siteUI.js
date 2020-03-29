7c036dcb9aee91f015591c79fee51caff53bccbf
kyle-n <kylebnazario@gmail.com>
2020-03-29 14:29:03 -0600

    Toggles icons on touches

--- static/js/siteUI.js
+++ static/js/siteUI.js
@@ -3,22 +3,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // -------------------------------------------------------------------------
   // toggles outline -> filled -> outline icons
+  const goSolid = link => {
+    const linkIcon = link.querySelector('i.material-icons-outlined');
+    if (linkIcon) {
+      linkIcon.classList.remove('material-icons-outlined');
+      linkIcon.classList.add('material-icons');
+    }
+  };
+  const goOutlined = link => {
+    const linkIcon = link.querySelector('i.material-icons');
+    if (linkIcon) {
+      linkIcon.classList.add('material-icons-outlined');
+      linkIcon.classList.remove('material-icons');
+    }
+  };
+
   const links = document.querySelectorAll('a');
   links.forEach(link => {
-    link.addEventListener('mouseenter', () => {
-      const linkIcon = link.querySelector('i.material-icons-outlined');
-      if (linkIcon) {
-        linkIcon.classList.remove('material-icons-outlined');
-        linkIcon.classList.add('material-icons');
-      }
-    });
-    link.addEventListener('mouseleave', () => {
-      const linkIcon = link.querySelector('i.material-icons');
-      if (linkIcon) {
-        linkIcon.classList.add('material-icons-outlined');
-        linkIcon.classList.remove('material-icons');
-      }
-    })
+    link.addEventListener('mouseenter', () => goSolid(link));
+    link.addEventListener('mouseleave', () => goOutlined(link));
+    link.addEventListener('touchstart', () => goSolid(link))
+    link.addEventListener('touchend', () => goOutlined(link))
   });
   // -------------------------------------------------------------------------
 
