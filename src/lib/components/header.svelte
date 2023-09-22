57595f2dee072aa56bc96b64470086b7c88edd6b
Kyle Nazario <knazario@bitovi.com>
2023-09-22 14:24:35 -0400

    Initial routing and components

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,9 @@
+<header>
+  <h1>Kyle Nazario's Blog</h1>
+  <nav>
+    <a href="/">Home</a> | 
+    <a href="/about">About</a> | 
+    <a href="/blog">Blog</a> | 
+    <a href="/contact">Contact</a>
+  </nav>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Header from '$lib/components/header.svelte';
+</script>
+
+<Header />
+
+<slot />
+
+<footer>
+  <p>© 2023 Kyle Nazario</p>
+</footer>
