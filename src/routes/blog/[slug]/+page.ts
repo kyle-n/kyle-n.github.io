df114ae346d2e4acec68aaabce18b7c3569697fc
Kyle Nazario <knazario@bitovi.com>
2023-09-22 15:15:02 -0400

    Removes date from slug

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -2,7 +2,7 @@ export async function load({ params }) {
   const slug: string = params.slug;
   const post = await import(`../${slug}.md`);
   const { title } = post.metadata;
-  const date = new Date(slug.slice(0, 10));
+  const date = new Date(post.metadata.date);
   date.setHours(date.getHours() + 6)
   const content = post.default;
   return {
