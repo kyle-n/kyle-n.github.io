e99febfb678eb39a5dad9edd2cb3a0696b49d364
Kyle Nazario <knazario@bitovi.com>
2023-09-22 16:20:22 -0400

    Fixes css error

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,9 +1,24 @@
 <header>
   <h1>Kyle Nazario</h1>
   <nav>
-    <a href="/">Home</a> |
-    <a href="/about">About</a> |
-    <a href="/blog">Blog</a> |
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
     <a href="/contact">Contact</a>
   </nav>
 </header>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: space-around;
+  }
+  
+  /* Fixes jump on hover */
+  nav a {
+    border-bottom: 2px solid transparent;
+  }
+  nav a:hover {
+    border-bottom: 2px solid #d9d8dc;
+  }
+</style>
