fc06a0dbde6aa50c782bb896ce9292dbaf6f4280
Kyle Nazario <knazario@bitovi.com>
2023-09-22 14:32:15 -0400

    Markdown processing

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  preprocess: [
+    vitePreprocess(),
+    mdsvex({
+      extension: '.md'
+    })
+  ],
+
+  extensions: ['.svelte', '.md'],
 
   kit: {
     adapter: adapter({
