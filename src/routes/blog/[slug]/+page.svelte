b1fbb9a40cb707ac637fd37e42c31e7fd5a8593e
Kyle Nazario <knazario@bitovi.com>
2023-09-23 06:03:38 -0400

    Unit testing post

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -8,7 +8,7 @@
   <h2>{data.metadata.title}</h2>
   <PostDate date={data.metadata.date} />
   {#if data.metadata.image}
-    <img src="/{data.metadata.image}" alt={data.metadata.title} />
+    <img src="/img/{data.metadata.image}" alt={data.metadata.title} />
   {/if}
   <div>
     <svelte:component this={data.content} />
