57e726ff3270252aaabf6dc41fa78bfe8462b443
Kyle Nazario <knazario@bitovi.com>
2023-09-23 05:55:31 -0400

    Divider in footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -2,6 +2,13 @@
   const copyrightYear = new Date().getFullYear();
 </script>
 
+<hr>
 <footer>
   <p>© {copyrightYear} Kyle Nazario</p>
 </footer>
+
+<style>
+  hr {
+    margin: 2rem 0;
+  }
+</style>
