0d9049c335c82bd5c9da7678ad5f06a441ad2223
Kyle Nazario <knazario@bitovi.com>
2023-09-23 06:31:34 -0400

    Sorts posts by date reverse chron

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -4,10 +4,16 @@ export async function load(): Promise<ResolvedBlogPostList> {
   const pathPrefix = './posts/';
   const allPostFiles = import.meta.glob('./posts/*.md');
   const iterablePostFiles = Object.entries(allPostFiles);
-  const posts = iterablePostFiles.map(async ([path, resolver]) => {
+  const postJobs = iterablePostFiles.map(async ([path, resolver]) => {
     const { metadata } = (await resolver()) as { metadata: BlogPostMetadata };
     const postPath = path.replace(pathPrefix, '').replace('.md', '');
     return { metadata, postPath };
   });
-  return { posts: await Promise.all(posts) };
+  const posts = await Promise.all(postJobs);
+  posts.sort((a, b) => {
+    const dateA = new Date(a.metadata.date);
+    const dateB = new Date(b.metadata.date);
+    return dateB.getTime() - dateA.getTime();
+  });
+  return { posts };
 }
