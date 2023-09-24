f5b0b2b42dc1484bc5de10daf9f81a02d7f0ebb6
Kyle Nazario <knazario@bitovi.com>
2023-09-24 08:15:28 -0400

    Separate fn for xml create

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -1,12 +1,17 @@
-import { error } from '@sveltejs/kit';
+import { getAllPosts } from '$lib/post-handlers'
 
-export async function GET({url}) {
+export async function GET() {
   const headers = {
     'Cache-Control': 'max-age=0, s-maxage=3600',
     'Content-Type': 'application/xml',
   }
   return new Response(
-    `<rss xmlns:dc="https://purl.org/dc/elements/1.1/" xmlns:content="https://purl.org/rss/1.0/modules/content/" xmlns:atom="https://www.w3.org/2005/Atom" version="2.0"></rss>`,
+    await getRssXml(),
     { headers }
   )
+}
+
+async function getRssXml(): Promise<string> {
+  const allPosts = await getAllPosts()
+  return ''
 }
