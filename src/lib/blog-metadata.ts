da5383fe2cda9c3a4b07e29498c117322261be1a
Kyle Nazario <knazario@bitovi.com>
2023-09-24 08:27:38 -0400

    RSS metadata

---
+++ src/lib/blog-metadata.ts
@@ -0,0 +1,3 @@
+export const BLOG_TITLE = 'Kyle Nazario';
+export const BLOG_URL = 'https://www.kylenazario.com';
+export const BLOG_DESCRIPTION = 'Frontend web developer, app tinkerer and TypeScript enthusiast.';
--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -1,17 +1,30 @@
-import { getAllPosts } from '$lib/post-handlers'
+import { BLOG_DESCRIPTION, BLOG_TITLE, BLOG_URL } from '$lib/blog-metadata';
+import { getAllPosts } from '$lib/post-handlers';
+import { create } from 'xmlbuilder2';
 
 export async function GET() {
   const headers = {
     'Cache-Control': 'max-age=0, s-maxage=3600',
-    'Content-Type': 'application/xml',
-  }
-  return new Response(
-    await getRssXml(),
-    { headers }
-  )
+    'Content-Type': 'application/xml'
+  };
+  return new Response(await getRssXml(), { headers });
 }
 
 async function getRssXml(): Promise<string> {
-  const allPosts = await getAllPosts()
-  return ''
+  const allPosts = await getAllPosts();
+  const root = create({ version: '1.0' }).ele('rss', {
+    'xmlns:dc': 'https://purl.org/dc/elements/1.1/',
+    'xmlns:content': 'https://purl.org/rss/1.0/modules/content/',
+    'xmlns:atom': 'https://www.w3.org/2005/Atom',
+    version: '2.0'
+  })
+  .ele('channel')
+  .ele('title').txt(BLOG_TITLE).up()
+  .ele('link').txt(BLOG_URL).up()
+  .ele('description').txt(BLOG_DESCRIPTION).up()
+  .ele('language').txt('en').up()
+  .ele('lastBuildDate').txt(new Date().toUTCString()).up()
+  .up()
+  .up();
+  return root.toString();
 }
