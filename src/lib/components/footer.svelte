d8af9bb24132eb8e1d4901780c39c61b9266b0ac
Kyle Nazario <knazario@bitovi.com>
2023-09-25 11:09:05 -0400

    RSS link in footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,13 +1,20 @@
 <script>
+  import RssLink from './rss-link.svelte';
+
   const copyrightYear = new Date().getFullYear();
 </script>
 
 <hr />
 <footer>
   <p>© {copyrightYear} Kyle Nazario</p>
+  <RssLink />
 </footer>
 
 <style>
+  footer {
+    display: flex;
+    justify-content: space-between;
+  }
   hr {
     margin: 2rem 0;
   }
