2aa836de3dd7eacd2bd3764b5c667daa21077014
Kyle Nazario <knazario@bitovi.com>
2023-09-25 16:23:55 -0400

    Keyword list component

---
+++ src/lib/components/keyword-links.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let keywords: string | undefined;
+
+  let keywordList = new Set<string>();
+  $: {
+    if (keywords) {
+      keywordList = new Set(keywords.split(',').map(keyword => keyword.trim()));
+    }
+  }
+</script>
+
+{#if keywords}
+  <div class="keyword-links">
+    {#each keywordList as keyword}
+      <a href="/blog/keyword/{keyword}">#{keyword}</a>
+    {/each}
+  </div>
+{/if}
+
+<style lang="scss">
+  .keyword-links {
+    display: flex;
+    a {
+      display: block;
+      margin-right: 1.5rem;
+    }
+  }
+</style>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import PageTitle from '$lib/components/page-title.svelte';
+  import KeywordLinks from '$lib/components/keyword-links.svelte';
+import PageTitle from '$lib/components/page-title.svelte';
   import PostDate from '$lib/components/post-date.svelte';
   import RelatedPosts from '$lib/components/related-posts.svelte';
   import type { ResolvedBlogPost } from '$lib/types';
@@ -12,6 +13,7 @@
   <div id="title-and-date">
     <h2 id="post-title">{data.metadata.title}</h2>
     <PostDate date={data.metadata.date} />
+    <KeywordLinks keywords={data.metadata.keywords} />
   </div>
   {#if data.metadata.image}
     <img src="/img/{data.metadata.image}" alt={data.metadata.title} />
