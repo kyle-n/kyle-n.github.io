c252175ff7426a6363b1bc39241108bb18a6d8d3
Kyle Nazario <knazario@bitovi.com>
2023-09-25 16:41:46 -0400

    RSS icon at top of blog

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import PageTitle from '$lib/components/page-title.svelte';
   import PostList from '$lib/components/post-list.svelte';
+  import RssLink from '$lib/components/rss-link.svelte';
   import type { ResolvedBlogPostList } from '$lib/types';
 
   export let data: ResolvedBlogPostList;
@@ -8,5 +9,22 @@
 
 <PageTitle title="Blog" />
 
-<h2>Blog</h2>
+<div id="blog-page-header">
+  <h2>Blog</h2>
+  <div>
+    <RssLink />
+  </div>
+</div>
 <PostList posts={data.posts} />
+
+<style lang="scss">
+  #blog-page-header {
+    display: flex;
+    justify-content: space-between;
+
+    div {
+      align-self: flex-end;
+      margin-bottom: 15px;
+    }
+  }
+</style>
