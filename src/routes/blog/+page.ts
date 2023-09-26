271444605b01ef49c2681de6c660cad85c7518d0
Kyle Nazario <knazario@bitovi.com>
2023-09-26 09:25:16 -0400

    Default posts per page

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -5,13 +5,10 @@ import type { ResolvedBlogPostList, BlogPostMetadata } from '$lib/types';
 export async function load({ url }): Promise<ResolvedBlogPostList> {
   const posts = await getAllPosts();
   const currentPage = Number(url.searchParams.get('page') ?? 1);
-  const postsPerPage = Number(
-    url.searchParams.get('postsPerPage') ?? DEFAULT_POSTS_PER_PAGE
-  );
-  const startIndex = (currentPage - 1) * postsPerPage;
-  const totalPageCount = Math.ceil(posts.length / postsPerPage);
+  const startIndex = (currentPage - 1) * DEFAULT_POSTS_PER_PAGE;
+  const totalPageCount = Math.ceil(posts.length / DEFAULT_POSTS_PER_PAGE);
   return {
-    posts: posts.slice(startIndex, startIndex + postsPerPage),
+    posts: posts.slice(startIndex, startIndex + DEFAULT_POSTS_PER_PAGE),
     totalPageCount,
     currentPage
   };
