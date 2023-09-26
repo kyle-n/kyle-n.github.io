da3695a5dd2e1e4fb3c214872bb5088189734f59
Kyle Nazario <knazario@bitovi.com>
2023-09-26 15:03:32 -0400

    Omits RSS route

--- svelte.config.js
+++ svelte.config.js
@@ -23,7 +23,7 @@ const config = {
       assets: 'build',
       fallback: undefined,
       precompress: false,
-      strict: true
+      strict: false
     }),
     paths: {
       base: dev ? '' : process.env.BASE_PATH
