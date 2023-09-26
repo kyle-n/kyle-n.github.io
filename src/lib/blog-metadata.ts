5de5377550320dc9ca1305fc9ab9c5f559ecfeb9
Kyle Nazario <knazario@bitovi.com>
2023-09-26 09:14:22 -0400

    Paginates posts

--- src/lib/blog-metadata.ts
+++ src/lib/blog-metadata.ts
@@ -16,3 +16,5 @@ export const BLOG_AUTHOR_EMAIL = 'kylebnazario@gmail.com';
 export const BLOG_AUTHOR_LINKEDIN = 'kylenazario';
 export const BLOG_AUTHOR_MASTODON = '@kn';
 export const BLOG_AUTHOR_BLUESKY = 'kylenazario.com';
+
+export const DEFAULT_POSTS_PER_PAGE = 10;
--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,7 +1,11 @@
+import { DEFAULT_POSTS_PER_PAGE } from '$lib/blog-metadata.js';
 import { getAllPosts } from '$lib/post-handlers';
 import type { ResolvedBlogPostList, BlogPostMetadata } from '$lib/types';
 
-export async function load(): Promise<ResolvedBlogPostList> {
+export async function load({url}): Promise<ResolvedBlogPostList> {
   const posts = await getAllPosts();
-  return { posts };
+  const page = Number(url.searchParams.get('page') ?? 1);
+  const postsPerPage = Number(url.searchParams.get('postsPerPage') ?? DEFAULT_POSTS_PER_PAGE);
+  const startIndex = (page - 1) * postsPerPage;
+  return { posts: posts.slice(startIndex, startIndex + postsPerPage) };
 }
