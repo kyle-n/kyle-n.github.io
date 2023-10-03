fddda3b1475bb9f2cb672c00e2405f13b125f42c
Kyle Nazario <knazario@bitovi.com>
2023-10-03 09:18:14 -0400

    Description component

---
+++ src/lib/components/page-description.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let description: string;
+</script>
+
+<svelte:head>
+  <meta name="description" content={description} />
+  <meta property="og:description" content={description} />
+  <meta property="twitter:description" content={description} />
+</svelte:head>
