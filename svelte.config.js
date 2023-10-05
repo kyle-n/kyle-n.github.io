bdf390e935416f7b915b02060980638881df402a
Kyle Nazario <knazario@bitovi.com>
2023-10-05 10:05:31 -0400

    Prefixes image paths

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,16 @@ import { mdsvex } from 'mdsvex';
 import remarkFootnotes from 'remark-footnotes';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import imgLinks from '@pondorasti/remark-img-links'
 
 const dev = process.argv.includes('dev');
+const base = dev ? '' : process.env.BASE_PATH;
+let BLOG_URL;
+if (dev) {
+  BLOG_URL = 'http://localhost:5173';
+} else {
+  BLOG_URL = 'https://www.kylenazario.com';
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +21,9 @@ const config = {
     vitePreprocess(),
     mdsvex({
       extension: '.md',
-      remarkPlugins: [remarkFootnotes],
+      remarkPlugins: [remarkFootnotes, [
+        imgLinks, { absolutePath: BLOG_URL + '/img/'}
+      ]],
       rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
     })
   ],
@@ -29,7 +39,7 @@ const config = {
       strict: false
     }),
     paths: {
-      base: dev ? '' : process.env.BASE_PATH
+    base
     }
   }
 };
