3bfa4c7b67d1c7de8e3d067efe74ffd672d66a81
Kyle Nazario <knazario@bitovi.com>
2023-10-23 09:39:42 -0400

    Uses beforeEach

--- src/lib/components/footer.test.ts
+++ src/lib/components/footer.test.ts
@@ -1,13 +1,15 @@
-import { describe, test, expect } from 'vitest';
-import { render, screen, within } from '@testing-library/svelte';
+import { describe, test, expect, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
 import Footer from './footer.svelte';
 import 'vitest-dom/extend-expect';
 
 describe('Footer', () => {
-  test('displays copyright for current year', () => {
+  beforeEach(() => {
     render(Footer);
+  });
 
+  test('displays copyright for current year', () => {
     const currentYear = new Date().getFullYear();
-    expect(screen.getByTestId('copyright').textContent).toContain(currentYear)
+    expect(screen.getByTestId('copyright').textContent).toContain(currentYear);
   });
 });
