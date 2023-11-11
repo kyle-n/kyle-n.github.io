6b3612748d7f864021ab215caddc3a53f24ce8d9
Kyle Nazario <knazario@bitovi.com>
2023-11-11 16:02:19 -0500

    Removes globals

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
   },
   test: {
     environment: 'jsdom',
-    setupFiles: ['./src/tests/setup.ts'],
-    globals: true
+    setupFiles: ['./src/tests/setup.ts']
   }
 });
