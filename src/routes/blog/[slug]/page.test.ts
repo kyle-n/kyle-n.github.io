4a23090c5b340226e39f45d64c09c3a4cbc9df87
Kyle Nazario <knazario@bitovi.com>
2023-11-12 15:15:47 -0500

    Finishes unit testing

--- src/routes/blog/[slug]/page.test.ts
+++ src/routes/blog/[slug]/page.test.ts
@@ -1,6 +1,6 @@
 import Header from '$lib/components/header.svelte';
 import type { ResolvedBlogPost } from '$lib/types';
-import { render, screen } from '@testing-library/svelte';
+import { cleanup, render, screen } from '@testing-library/svelte';
 import { describe, expect, it, beforeEach } from 'vitest';
 import BlogPostPage from './+page.svelte';
 import { BLOG_TITLE } from '$lib/blog-metadata';
@@ -14,12 +14,12 @@ describe('Blog post page', () => {
         layout: 'post',
         title: 'Test post',
         description: 'Test description',
-        date: '2021-01-01',
+        date: '2021-01-01'
       },
-      content: Header,
+      content: Header
     };
 
-    render(BlogPostPage, {data: mockResolvedBlogPost});
+    render(BlogPostPage, { data: mockResolvedBlogPost });
   });
 
   it('should render the mock article component, the header', () => {
@@ -27,6 +27,29 @@ describe('Blog post page', () => {
   });
 
   it('should render the article title', () => {
-    expect(screen.getByText(mockResolvedBlogPost.metadata.title)).toBeInTheDocument();
+    expect(
+      screen.getByText(mockResolvedBlogPost.metadata.title)
+    ).toBeInTheDocument();
+  });
+
+  it('should not have an image with no lead image provided', () => {
+    expect(screen.queryByRole('img')).not.toBeInTheDocument();
+  });
+
+  it('should render the lead image', () => {
+    mockResolvedBlogPost.metadata.image = 'https://example.com/image.jpg';
+    cleanup();
+    render(BlogPostPage, { data: mockResolvedBlogPost });
+    expect(screen.getByRole('img')).toBeInTheDocument();
+  });
+
+  it('should render the image caption if present', () => {
+    mockResolvedBlogPost.metadata.image = 'https://example.com/image.jpg';
+    mockResolvedBlogPost.metadata.caption = 'Test caption';
+    cleanup();
+    render(BlogPostPage, { data: mockResolvedBlogPost });
+    expect(
+      screen.getByText(mockResolvedBlogPost.metadata.caption)
+    ).toBeInTheDocument();
   });
 });
