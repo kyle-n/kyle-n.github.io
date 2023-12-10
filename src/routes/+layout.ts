48b2a9852563b17eba5b03af9978e81f445575fd
Kyle Nazario <knazario@bitovi.com>
2023-12-10 14:36:21 -0500

    Installs Vercel speed insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
-
-inject({ mode: dev ? 'development' : 'production' });
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+ 
+inject({ mode: dev ? 'development' : 'production'})
+injectSpeedInsights();
 
 export const prerender = true;
