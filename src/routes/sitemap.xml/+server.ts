587ac7ce0c5b1fb4c9d2c05df06d643c531418fb
Kyle Nazario <knazario@bitovi.com>
2024-01-05 14:33:57 -0500

    Sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,40 @@
+import { BLOG_URL, DEFAULT_POSTS_PER_PAGE } from '$lib/blog-metadata';
+import { getAllPosts, getCorrectedPostDate } from '$lib/post-handlers';
+import { create } from 'xmlbuilder2';
+
+export const prerender = true;
+
+export async function GET() {
+  const headers = {
+    'Cache-Control': 'max-age=0, s-maxage=3600',
+    'Content-Type': 'application/xml'
+  };
+  return new Response(await getSitemapXml(), { headers });
+}
+
+// prettier-ignore
+async function getSitemapXml(): Promise<string> {
+  const allPosts = await getAllPosts();
+  const sitemapPosts = allPosts.slice(0, DEFAULT_POSTS_PER_PAGE);
+  const root = create({ version: '1.0', encoding: 'utf-8' })
+  .ele('urlset', {
+    xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9',
+    'xmlns:xhtml': 'http://www.w3.org/1999/xhtml',
+    'xmlns:mobile': 'http://www.google.com/schemas/sitemap-mobile/1.0',
+    'xmlns:image': 'http://www.google.com/schemas/sitemap-image/1.1',
+    'xmlns:news': 'http://www.google.com/schemas/sitemap-news/0.9',
+    'xmlns:video': 'http://www.google.com/schemas/sitemap-video/1.1',
+  })
+
+  for await (const post of sitemapPosts) {
+    const postUrl = `${BLOG_URL}/blog/${post.postPath}`;
+    const pubDate = getCorrectedPostDate(post.metadata.date);
+
+    root.ele('url')
+      .ele('loc').txt(postUrl).up()
+      .ele('lastmod').txt(pubDate).up()
+    .up();
+  }
+
+  return root.end()
+}
