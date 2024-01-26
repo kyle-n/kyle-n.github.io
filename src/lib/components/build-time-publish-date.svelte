6e5f300501f2f89d09ac549a82721fbedeb81c54
Kyle Nazario <knazario@bitovi.com>
2024-01-26 07:19:27 -0500

    Publish date meta tag

---
+++ src/lib/components/build-time-publish-date.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import PublishDate from './publish-date.svelte';
+
+  const now = new Date();
+</script>
+
+<PublishDate date={now} />
---
+++ src/lib/components/publish-date.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { getCorrectedPostDate } from '$lib/post-handlers';
+  import dayjs from 'dayjs';
+
+  export let date: Date | string;
+
+  const dateWithTimezoneOffset = getCorrectedPostDate(date);
+  const content = dayjs(dateWithTimezoneOffset).toISOString();
+</script>
+
+<svelte:head>
+  <meta property="og:publish_date" name="publish_date" {content} />
+</svelte:head>
