316c4b1269d07ab5cac06e521ec9f13a8cf42ebc
Kyle Nazario <knazario@bitovi.com>
2024-02-27 15:03:10 -0500

    YT Embed component

---
+++ src/lib/components/youtube-embed.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let id: string;
+
+  const src = `https://www.youtube-nocookie.com/embed/${id}`;
+</script>
+
+<iframe
+  width="560"
+  height="315"
+  {src}
+  title="YouTube video player"
+  frameborder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  allowfullscreen
+  loading="lazy"
+/>
