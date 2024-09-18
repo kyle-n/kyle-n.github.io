fcf6687b612552b77540d9d04b961545d753805b
Kyle Nazario <kylebnazario@gmail.com>
2024-09-18 17:30:36 -0600

    Video component

---
+++ src/lib/components/inline-video.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { base } from '$app/paths';
+
+  export let filename: string;
+
+  $: src = `${base}/video/${filename}`;
+</script>
+
+<div>
+  <video controls>
+    <source type="video/mp4" {src} />
+  </video>
+</div>
+
+<style>
+  div {
+    display: flex;
+    justify-content: center;
+  }
+  video {
+    width: 250px;
+    max-width: 100%;
+  }
+</style>
